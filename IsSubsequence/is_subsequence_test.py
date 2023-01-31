b08ce7918d093cf4283256690fe7dc4873c024ac
Huyen Pham <thanhhuyen2995@gmail.com>
2023-01-31 07:12:01 +0000

    done is_subsequence

--- IsSubsequence/is_subsequence_test.py
+++ IsSubsequence/is_subsequence_test.py
@@ -4,6 +4,6 @@ from  is_subsequence import is_subsequence
 @pytest.mark.parametrize("input1, input2, expected", [("abc", "ahbgdc", True),
                                             ("", "", True),
                                             ("", "ajdhsa", True),
-                                            ("axc", "ahbgdc")])
+                                            ("axc", "ahbgdc", False)])
 def testValidInput(input1, input2, expected):
     assert is_subsequence(input1, input2) == expected
